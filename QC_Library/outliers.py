416da0a27436168a17e29d6f14f5ddf1286269c9
oz <john.osborne@noaa.gov>
2019-02-05 16:41:04 -0800

    Added Verbosity property

--- QC_Library/outliers.py
+++ QC_Library/outliers.py
@@ -5,7 +5,7 @@ def outlierDetector(timeArray, data, flags, minWindowN=4, windowInDays=1):
     if data.ndim != 1 or timeArray.ndim != 1 or flags.ndim != 1:
         raise ValueError("Inputs must be 1D")
     variances = np.zeros_like(data)
-    
+
     if windowInDays != 'all':
         deltaTime = np.timedelta64(windowInDays, 'D')
     windowsN = np.zeros_like(data, dtype=np.int)
@@ -34,11 +34,11 @@ def outlierDetector(timeArray, data, flags, minWindowN=4, windowInDays=1):
         numDevs[indx] = (data[indx] - means[indx])/np.sqrt(variances[indx])
     return numDevs, flags
 
-def outlierRemoval(timeArray, data, windowInDays=1, minWindowN=4, numStdDevs=5, maxIterations=None, flags=None):
+def outlierRemoval(timeArray, data, windowInDays=1, minWindowN=4, numStdDevs=5, maxIterations=None, flags=None, verbosity=0):
     # create the flags array
     if flags is None:
         flags = np.zeros_like(data, dtype=np.int) + 2
-    
+
     # detect first outlier
     #print(np.where(flags != 2)[0].size)
     numDevs,flags = outlierDetector(timeArray, data, flags, minWindowN=minWindowN, windowInDays=windowInDays)
@@ -46,8 +46,9 @@ def outlierRemoval(timeArray, data, windowInDays=1, minWindowN=4, numStdDevs=5, 
     if (maxIterations is None):
         maxIterations = data.size
     while ((np.nanmax(np.abs(numDevs)) > numStdDevs) and (cnt < maxIterations)):
-        #print(cnt)
-        #print(np.where(flags != 2)[0].size)
+    	if (verbosity > 0):
+        	print(cnt)
+        	print(np.where(flags != 2)[0].size)
         flags[np.where(np.abs(numDevs) == np.nanmax(np.abs(numDevs)))] = 4
 
         numDevs,flags = outlierDetector(timeArray, data, flags, minWindowN=minWindowN, windowInDays=windowInDays)
