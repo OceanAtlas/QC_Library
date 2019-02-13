45265ebf6b3707aacdc0a584b2889d367ef75998
oz <john.osborne@noaa.gov>
2019-02-13 15:41:05 -0800

    Bug fixes, new Gross Range Test

--- QC_Library/outliers.py
+++ QC_Library/outliers.py
@@ -7,7 +7,7 @@ def outlierDetector(timeArray, data, flags, minWindowN=4, windowInDays=1):
     variances = np.zeros_like(data)
 
     if windowInDays != 'all':
-        deltaTime = np.timedelta64(windowInDays, 'D')
+        deltaTime = pd.Timedelta(windowInDays, 'D').to_timedelta64()
     windowsN = np.zeros_like(data, dtype=np.int)
     means = np.zeros_like(data)
     numDevs = np.zeros_like(data)
@@ -32,27 +32,49 @@ def outlierDetector(timeArray, data, flags, minWindowN=4, windowInDays=1):
 #         variances[indx] = np.nanvar(data[window])
         means[indx] = np.nanmean(data[window])
         numDevs[indx] = (data[indx] - means[indx])/np.sqrt(variances[indx])
-    return numDevs, flags
+        #return dict
+        retdict = {'numdevs':numDevs, 'flags': flags, 'means': means, 'variances':variances}
+    return retdict
 
 def outlierRemoval(timeArray, data, windowInDays=1, minWindowN=4, numStdDevs=5, maxIterations=None, flags=None, verbosity=0):
     # create the flags array
     if flags is None:
-        flags = np.zeros_like(data, dtype=np.int) + 2
+        localflags = np.zeros_like(data, dtype=np.int) + 2
+    else:
+        localflags = np.copy(flags)
 
     # detect first outlier
     #print(np.where(flags != 2)[0].size)
-    numDevs,flags = outlierDetector(timeArray, data, flags, minWindowN=minWindowN, windowInDays=windowInDays)
+    aDict = outlierDetector(timeArray, data, localflags, minWindowN=minWindowN, windowInDays=windowInDays)
+    numDevs = aDict['numdevs']
+    localflags = aDict['flags']
+
     cnt = 0
     if (maxIterations is None):
         maxIterations = data.size
     while ((np.nanmax(np.abs(numDevs)) > numStdDevs) and (cnt < maxIterations)):
-    	if (verbosity > 0):
-        	print(cnt)
-        	print(np.where(flags != 2)[0].size)
-        flags[np.where(np.abs(numDevs) == np.nanmax(np.abs(numDevs)))] = 4
+        if verbosity > 0:
+            print(cnt, " ", np.where(localflags != 2)[0].size," points flagged")
 
-        numDevs,flags = outlierDetector(timeArray, data, flags, minWindowN=minWindowN, windowInDays=windowInDays)
-
+        localflags[np.where(np.abs(numDevs) > numStdDevs)] = 4
+        aDict = outlierDetector(timeArray, data, localflags, minWindowN=minWindowN, windowInDays=windowInDays)
+        numDevs = aDict['numdevs']
+        localflags = aDict['flags']
         cnt += 1
-    return flags, numDevs
+    print(cnt, " ", np.where(localflags != 2)[0].size," points flagged")
+    retdict = {'numdevs':numDevs, 'flags': localflags, 'means': aDict['means'], 'variances':aDict['variances']}
+    return retdict
 
+def grossRangeFlag(timeArray, data, min, max, flags=None):
+    if data.ndim != 1 or timeArray.ndim != 1:
+        raise ValueError("Inputs must be 1D")
+    if flags is None:
+        localflags = np.zeros_like(data, dtype=np.int) + 2
+    else:
+        localflags = np.copy(flags)
+
+    # iterate
+    for indx,time in enumerate(timeArray):
+        if data[indx] < min or data[indx] > max:
+            localflags[indx] = 4
+    return localflags
